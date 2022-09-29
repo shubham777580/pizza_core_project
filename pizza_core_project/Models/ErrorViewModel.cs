f15358bd6eed88f4f2dbe4bac6584b0485ac78c3
shubham777580 <shubhammukherjee370@gmail.com>
2022-09-29 14:41:27 +0530

    Add project files.

---
+++ pizza_core_project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pizza_core_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
